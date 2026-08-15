1ec493f26242a044100b6737740e0093e727e76f
Samirborges <alessandro.samir@souunit.com.br>
2026-08-14 22:44:35 -0300

    feat(cto_agent): Adding CTO agent

--- backend/app/agents/orchestrator.py
+++ backend/app/agents/orchestrator.py
@@ -1,7 +1,9 @@
 # Orquestrador será responsável por controlar o fluxo do motor RAG e decidir quais agentes serão acionados
 from app.agents.ceo_agent import CEOAgent
+from app.agents.cto_agent import CTOAgent
 
 
 chain_agents: dict = {
-        "ceo_agent": CEOAgent()
+        "ceo_agent": CEOAgent(),
+        "cto_agent": CTOAgent()
     }
