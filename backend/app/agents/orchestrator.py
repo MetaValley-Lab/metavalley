3b2984b89042101316147b80acfaba4b54cdcfd7
Samirborges <alessandro.samir@souunit.com.br>
2026-08-14 22:53:19 -0300

    feat(cfo_agent): Adding CFO agent

--- backend/app/agents/orchestrator.py
+++ backend/app/agents/orchestrator.py
@@ -1,9 +1,11 @@
 # Orquestrador será responsável por controlar o fluxo do motor RAG e decidir quais agentes serão acionados
 from app.agents.ceo_agent import CEOAgent
 from app.agents.cto_agent import CTOAgent
+from app.agents.cfo_agent import CFOAgent
 
 
 chain_agents: dict = {
         "ceo_agent": CEOAgent(),
-        "cto_agent": CTOAgent()
+        "cto_agent": CTOAgent(),
+        "cfo_agent": CFOAgent(),
     }
